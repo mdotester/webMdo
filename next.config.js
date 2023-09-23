cfc5277ea897854a1c596875ea372532da21810f
irwin-shauma <irwin.shauma.rizky@gmail.com>
2023-09-23 22:17:55 +0700

    Commit Irwin 23 Sept 2023

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig: {
-    // apiCloseUrl: "http://localhost:3131",
-    apiCloseUrl: "http://172.18.141.41:3131",
+    apiCloseUrl: "http://localhost:5000",
+    // apiCloseUrl: "http://172.18.141.41:3131",
     endpointAD: "/login/",
     endpointCreateAlert: "/createAlert/",
     endpointEsbGet: "/esbGet/",
